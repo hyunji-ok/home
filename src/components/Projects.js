21d0906c85767db389ff7bbca447124392885d70
okhj159 <okhj159@gmail.com>
2024-10-24 15:07:23 +0900

    feat: project component 이미지 변경 및 css 수정(scroll-behavior, about radius)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,9 @@ import React from 'react';
 import project1 from '../images/project1_1280.jpg'
 import project2 from '../images/project2_1280.jpg'
 import project3 from '../images/project3_1280.jpg'
+import project4 from '../images/project4_1280.jpg'
+import project5 from '../images/project5_1280.jpg'
+import project6 from '../images/project6_1280.jpg'
 
 function Projects() {
   return (
@@ -24,17 +27,17 @@ function Projects() {
           <p>Description of the project...</p>
         </div>
         <div className="card">
-          <img src={project1} alt="Project 4" />
+          <img src={project4} alt="Project 4" />
           <h3>Project 4</h3>
           <p>Description of the project...</p>
         </div>
         <div className="card">
-          <img src={project2} alt="Project 5" />
+          <img src={project5} alt="Project 5" />
           <h3>Project 5</h3>
           <p>Description of the project...</p>
         </div>
         <div className="card">
-          <img src={project3} alt="Project 6" />
+          <img src={project6} alt="Project 6" />
           <h3>Project 6</h3>
           <p>Description of the project...</p>
         </div>
