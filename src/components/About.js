acc2ac6ec9a2d235957a56f72551ea37fb24d13c
okhj159 <okhj159@gmail.com>
2024-10-24 17:14:38 +0900

    feat: about component 및 css 수정

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,12 @@ import about from '../images/about_1280.jpg'
 function About() {
   return (
     <div className="about">
-      <img src={about} alt="Profile" />
-      <div className="about-text">
-        <h2>About Me</h2>
-        <p>프론트엔드 개발자입니다.</p>
-
-        <div>경력사항</div>
-        <ul>
-          <li>성원 애드피아</li>
-          <li>플래티어</li>
-        </ul>
+      <h2>About Me</h2>
+      <div className='about-item'>
+        <img src={about} alt="Profile" />
+        <div className="about-text">
+          <p>프론트엔드 개발자입니다.</p>
+        </div>
       </div>
     </div>
   );
