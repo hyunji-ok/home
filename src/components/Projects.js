a840d292a578cfd804f563be96e7b79c03d8ef72
okhj159 <okhj159@gmail.com>
2024-10-25 13:28:29 +0900

    feat: project component 수정

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import project1 from '../images/project1_1280.jpg'
 import project2 from '../images/project2_1280.jpg'
-import project3 from '../images/project3_1280.jpg'
-import project4 from '../images/project4_1280.jpg'
-import project5 from '../images/project5_1280.jpg'
-import project6 from '../images/project6_1280.jpg'
+// import project3 from '../images/project3_1280.jpg'
+// import project4 from '../images/project4_1280.jpg'
+// import project5 from '../images/project5_1280.jpg'
+// import project6 from '../images/project6_1280.jpg'
 
 function Projects() {
   return (
@@ -21,7 +21,7 @@ function Projects() {
           <h3>Project 2</h3>
           <p>Description of the project...</p>
         </div>
-        <div className="card">
+        {/* <div className="card">
           <img src={project3} alt="Project 3" />
           <h3>Project 3</h3>
           <p>Description of the project...</p>
@@ -40,7 +40,7 @@ function Projects() {
           <img src={project6} alt="Project 6" />
           <h3>Project 6</h3>
           <p>Description of the project...</p>
-        </div>
+        </div> */}
         {/* Add more cards as needed */}
       </div>
     </div>
