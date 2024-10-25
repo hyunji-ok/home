7e0444c0b139240352614b3252eb0499433dfc27
okhj159 <okhj159@gmail.com>
2024-10-25 14:55:35 +0900

    feat: dialog sample 작업

--- src/components/Carriers.js
+++ src/components/Carriers.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import carrier1 from '../images/carrier1.png'
 import carrier2 from '../images/carrier2.png'
+import AlertDialog from './common/dialog';
 
 function Carriers() {
   return (
     <div className="carrier">
         <h2>Carriers</h2>
         <div className="carrier-cards">
-            <div className="card">
-                <img src={carrier1} alt="carrier 1" />
-                <h3>성원애드피아</h3>
-            </div>
-            <div className="card">
-                <img src={carrier2} alt="carrier 2" />
-                <h3>플래티어</h3>
-            </div>
+            <AlertDialog>
+              <div className="card">
+                  <img src={carrier1} alt="carrier 1" />
+                  <h3>성원애드피아</h3>
+              </div>
+            </AlertDialog>
+            
+            <AlertDialog>
+              <div className="card">
+                  <img src={carrier2} alt="carrier 2" />
+                  <h3>플래티어</h3>
+              </div>
+            </AlertDialog>
         </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import project1 from '../images/project1_1280.jpg'
 import project2 from '../images/project2_1280.jpg'
+import AlertDialog from './common/dialog';
 // import project3 from '../images/project3_1280.jpg'
 // import project4 from '../images/project4_1280.jpg'
 // import project5 from '../images/project5_1280.jpg'
@@ -11,16 +12,22 @@ function Projects() {
     <div className="projects">
       <h2>My Projects</h2>
       <div className="project-cards">
-        <div className="card">
-          <img src={project1} alt="Project 1" />
-          <h3>Project 1</h3>
-          <p>Description of the project...</p>
-        </div>
-        <div className="card">
-          <img src={project2} alt="Project 2" />
-          <h3>Project 2</h3>
-          <p>Description of the project...</p>
-        </div>
+        <AlertDialog>
+          <div className="card">
+            <img src={project1} alt="Project 1" />
+            <h3>Project 1</h3>
+            <p>Description of the project...</p>
+          </div>
+        </AlertDialog>
+        
+        <AlertDialog>
+          <div className="card">
+            <img src={project2} alt="Project 2" />
+            <h3>Project 2</h3>
+            <p>Description of the project...</p>
+          </div>
+        </AlertDialog>
+        
         {/* <div className="card">
           <img src={project3} alt="Project 3" />
           <h3>Project 3</h3>
