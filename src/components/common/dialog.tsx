b30f0fc640668ac6528bd51104dc1122279a9b01
okhj159 <okhj159@gmail.com>
2024-10-28 15:12:29 +0900

    feat: js -> ts 변경

--- src/components/common/dialog.tsx
+++ src/components/common/dialog.tsx
@@ -6,7 +6,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
-export default function AlertDialog({children}) {
+export default function AlertDialog({children}: any) {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
