ec382e62b53dfb9457f655a91369852f7f4e8335
okhj159 <okhj159@gmail.com>
2024-10-28 16:46:36 +0900

    feat: personalhomepage component sample 작성

---
+++ src/routes/products/PersonalHomepage.tsx
@@ -0,0 +1,88 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React from 'react';
+// import Home from '../../components/main/Home';
+import About from '../../components/main/About';
+
+const sections = [
+    { id: 'about', component: About }
+];
+  
+function PersonalHomepage() {
+    return (
+      <div>
+        <header css={headerStyle}>
+          <nav>
+            <ul>
+              {sections.map(({ id, component }) => (
+                <li key={id}><a href={`#${id}`}>{id.charAt(0).toUpperCase() + id.slice(1)}</a></li>
+              ))}
+            </ul>
+          </nav>
+        </header>
+  
+        <main css={mainStyle}>
+          {sections.map(({ id, component: Component }) => (
+            <section key={id} id={id}>
+              <Component />
+            </section>
+          ))}
+        </main>
+  
+        <footer css={footerStyle}>
+          <div>Copyright © hyunji-ok</div>
+        </footer>
+      </div>
+    );
+  }
+  
+  const headerStyle = css`
+    /* max-width: var(--max-width);
+    margin: auto; */
+    
+    nav ul {
+      display: flex;
+      justify-content: center;
+      list-style-type: none;
+      padding: 0;
+    }
+  
+    nav ul li {
+      padding: 5px;
+      margin: 0 5px;
+      font-size: 1.5rem;
+    }
+  
+    nav a {
+      color: white;
+      text-decoration: none;
+      transition: color 0.3s ease;
+  
+      &:hover {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+  `;
+  
+  const mainStyle = css`
+    max-width: var(--max-width);
+    margin: auto;
+    padding-top: 60px; // 고정 헤더를 위한 여백 추가
+    height: 85vh;
+  
+    section {
+      padding: 60px 0 0;
+      text-align: center;
+    }
+  `;
+  
+  const footerStyle = css`
+    /* max-width: var(--max-width); */
+    margin: 30px auto 0;
+    display: flex;
+    div {
+      margin: auto;
+    }
+  `;
+
+export default PersonalHomepage
