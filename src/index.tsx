25a92404db4903882617327f0cfe8058961432ec
okhj159 <okhj159@gmail.com>
2024-10-30 09:05:08 +0900

    feat: 안쓰는 image 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -30,8 +30,9 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
